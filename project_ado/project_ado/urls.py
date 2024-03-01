4c1300e08fa434abeda3b7af617a0fa7aef1324b
Shane-Donlon <shane.donlon@live.com>
2024-03-01 12:44:19 +0000

    Fix: media handler for urls add

--- project_ado/project_ado/urls.py
+++ project_ado/project_ado/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
@@ -7,7 +9,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/", include("allauth.urls")),
     path("", include("unauth.urls")),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 handler404 = "project_ado.views.handler404"
 handler500 = "project_ado.views.handler500"
